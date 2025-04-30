2f0ba361b6b941084383549af5b311181524b480
José Luís <peressousa77@gmail.com>
2025-04-30 10:22:59 -0300

    Create salario.py

---
+++ lista-1/salario.py
@@ -0,0 +1,9 @@
+def salario(horas_trabalhadas: float) -> float:
+    '''O programa deve calcular o salário de um engenheiro
+    baseado no valor de R$34,00/hora
+    Exemplos:
+    >>> salario(120)
+    4080
+    >>> salario(200)
+    6800'''
+    return horas_trabalhadas * 34
