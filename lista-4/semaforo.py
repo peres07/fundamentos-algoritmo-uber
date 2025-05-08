5ecadb1926544224494a0168d814cd0dba18f9d9
José Luís <peressousa77@gmail.com>
2025-05-08 14:22:09 -0300

    Atualizar o semaforo.py

--- lista-4/semaforo.py
+++ lista-4/semaforo.py
@@ -10,14 +10,14 @@ def cor_semaforo(cor: Cor) -> Cor:
     '''Dada um cor atual de um semáforo, retorne a próxima cor
     Exemplos:
     >>> cor_semaforo(Cor.VERDE).name
-    'VERMELHO'
-    >>> cor_semaforo(Cor.VERMELHO).name
     'AMARELO'
-    >>> cor_semaforo(Cor.AMARELO).name
+    >>> cor_semaforo(Cor.VERMELHO).name
     'VERDE'
+    >>> cor_semaforo(Cor.AMARELO).name
+    'VERMELHO'
     '''
     if cor == Cor.VERDE:
-        return Cor.VERMELHO
-    elif cor == Cor.VERMELHO:
         return Cor.AMARELO
-    return Cor.VERDE
+    elif cor == Cor.VERMELHO:
+        return Cor.VERDE
+    return Cor.VERMELHO
